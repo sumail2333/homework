25a5f4d254166f1c2e586aa351bfefd8903c057a
swu33 <shuang.wu2@dxc.com>
2019-01-17 11:56:00 +0800

    roleMenu  backend

---
+++ src/main/java/pojo/RoleMenuBean.java
@@ -0,0 +1,43 @@
+package pojo;
+
+public class RoleMenuBean {
+    private Integer roleId;
+
+    private Integer menuId;
+
+    private String roleName;
+
+    private String menuName;
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public Integer getMenuId() {
+        return menuId;
+    }
+
+    public void setMenuId(Integer menuId) {
+        this.menuId = menuId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public void setMenuName(String menuName) {
+        this.menuName = menuName;
+    }
+}
